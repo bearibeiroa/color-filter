0b6cb66cea15b83f9beb4f66fc7e28daab796336
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-10-14 19:09:36 +0100

    gabarito

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders input', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputElement = screen.getByLabelText('Digite uma cor:');
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('renders colors in a list', () => {
+  render(<App />);
+  const red = screen.getAllByTestId('color');
+  expect(red).toHaveLength(7);
+});
+
+test('renders color during the filter', () => {
+  render(<App />);
+  const inputElement = screen.getByLabelText('Digite uma cor:');
+  expect(inputElement).toBeInTheDocument();
+
+  userEvent.type(inputElement, 'green');
+  const colorList = screen.getByTestId('color');
+  expect(colorList).toBeInTheDocument();
 });
