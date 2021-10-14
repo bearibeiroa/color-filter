d4ae7e4073e2dce4e4a404cb3853f41479c1fda6
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-10-14 19:11:23 +0100

    exercicio

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,25 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
-import App from '../App';
 
-test('renders input', () => {
-  render(<App />);
-  const inputElement = screen.getByLabelText('Digite uma cor:');
-  expect(inputElement).toBeInTheDocument();
+test('testa aplicação', () => {
+  //escreva seu código aqui...
 });
 
-test('renders colors in a list', () => {
-  render(<App />);
-  const red = screen.getAllByTestId('color');
-  expect(red).toHaveLength(7);
-});
-
-test('renders color during the filter', () => {
-  render(<App />);
-  const inputElement = screen.getByLabelText('Digite uma cor:');
-  expect(inputElement).toBeInTheDocument();
-
-  userEvent.type(inputElement, 'green');
-  const colorList = screen.getByTestId('color');
-  expect(colorList).toBeInTheDocument();
-});
